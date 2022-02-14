b1b4d6fe504e425b2f518df0f7d3c62854d87d64
CSolDev <99368509+CSolDev@users.noreply.github.com>
2022-02-14 05:02:47 -0300

    Add files via upload

---
+++ Assets/SceneHololens/UImanager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UImanager : MonoBehaviour
+{
+    public void Active()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Disable()
+    {
+        gameObject.SetActive(false);
+    }
+}
